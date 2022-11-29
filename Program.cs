955eb9ae91f7917b25596dd6b35b19adb741f918
Rohan Angne <Rohan.Angne@GEMINI.NCIRCLETECH.COM>
2022-11-29 22:27:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            string name = Input_display("Please enter your name");
+            string fav_phrase =Input_display("Please enter your favourite phrase");
+
+            Display_output(name, fav_phrase);
+            Console.ReadLine();
+        }
+        public  static string Input_display(string msg)
+        {
+            Console.WriteLine(msg);
+            return Console.ReadLine();
+        }
+
+        public static void Display_output(string name, string phrase)
+        {
+            Console.WriteLine("<><><><><><><><><><><><><><><><>");
+            Console.WriteLine("Your name is "+name);
+            Console.WriteLine("Your favourite phrase is " + phrase);
+            Console.WriteLine("<><><><><><><><><><><><><><><><>");
+        }
+    }
+}
